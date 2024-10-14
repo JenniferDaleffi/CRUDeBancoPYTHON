ab21442baecfb8dbda8fb4af6de49aa40debb9eb
JenniferDaleffi <jenniferdaleffii@gmail.com>
2024-10-14 11:01:36 -0300

    criando função inserir <insert>

--- import oracledb.py
+++ import oracledb.py
@@ -2,8 +2,11 @@ import oracledb
 
 #função com Oracle Database
 def get_connection():
-    connection = oracledb.connect('rm557137/020106@oracle.fiap.com.br:1521/orcl')
-    print('Conectado com o Oracle DB')
+    try:
+        connection = oracledb.connect('rm557137/020106@oracle.fiap.com.br:1521/orcl')
+        print('Conectado com o Oracle DB')
+    except Exception as e:
+        print(f'error obtaining a conncetion: {e}')
 return connection
 
 #função select
@@ -16,6 +19,22 @@ def listar():
     for linha in cursor:
         print(linha)
 
-# Connection.commit()
-cursor.close()
-connection.close()
+    cursor.close()
+    connection.close()
+
+# Função Insert 
+def inserir():
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql = "INSERT INTO <tabela> VALUES (1, 'Jennifer', 'Vieira')"
+    cursor.execute(sql)
+    connection.commit()
+    print('Ddods Inseridos!')
+    cursor.close()
+    connection.close()
+
+
+def inserir_param(id, nome, sobrenome):
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql = "INSERT INTO <tabela> (id, nome, sobrenome) VALUES({id}, {nome}, {sobrenome})"
